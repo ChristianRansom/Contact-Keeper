e70efb948a7b8bc2aefa789a7cc90a74172c6738
Christian Ransom <christian.ransom123@gmail.com>
2022-01-20 12:32:35 -0800

    Add fade in and out affect to contacts

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,35 @@ import React, { Fragment } from "react";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alerts from "./components/layout/Alerts";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment className='App'>
-          <Navbar />
-          <div className='container'>
-            <Routes>
-              <Route path='/' element={<Home />}></Route>
-              <Route path='/about' element={<About />}></Route>
-            </Routes>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment className='App'>
+              <Navbar />
+              <div className='container'>
+                <Alerts>
+                  <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/about' element={<About />}></Route>
+                    <Route path='/register' element={<Register />}></Route>
+                    <Route path='/login' element={<Login />}></Route>
+                  </Routes>
+                </Alerts>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 };
 
