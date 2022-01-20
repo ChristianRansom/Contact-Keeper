ee651b3d804b2a8c5c27a04bd85ed2223fad70ef
Christian Ransom <christian.ransom123@gmail.com>
2022-01-20 12:32:17 -0800

    Add fade in and out affec to contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useContext } from "react";
 import ContactItem from "./ContactItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactContext from "../../context/contact/contactContext";
 
 export const Contacts = () => {
@@ -12,13 +13,19 @@ export const Contacts = () => {
   }
   return (
     <Fragment>
-      {filtered !== null
-        ? filtered.map((contact) => (
-            <ContactItem contact={contact} key={contact.id} />
-          ))
-        : contacts.map((contact) => (
-            <ContactItem contact={contact} key={contact.id}></ContactItem>
-          ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact}></ContactItem>
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
